50fa5969a47b8809ebf3b829654c485b25512fea
10K920 <80634597+10K920@users.noreply.github.com>
2022-12-06 02:00:56 +0000

    name_change

--- MovieClean/MovieClean/data_type.py
+++ MovieClean/MovieClean/data_type.py
@@ -1,4 +1,4 @@
-def cols_to_float(meta_data_df):
+def budget_revenue_cols_to_float(meta_data_df):
     meta_data_df['budget'] = meta_data_df['budget'].astype('float')
     meta_data_df['revenue'] = meta_data_df['revenue'].astype('float')
     
